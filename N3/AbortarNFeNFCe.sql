a0c688287f66a24f8ba71ab5ef31197f82b57ff5
n3vendasmeta-cmd <n3vendasmeta@gmail.com>
2026-07-17 09:08:10 -0300

    Create AbortarNFeNFCe.sql

---
+++ N3/AbortarNFeNFCe.sql
@@ -0,0 +1,6 @@
+-- ABORTAR NFE E NFCE
+DECLARE @idDoc BIGINT = 0;
+UPDATE DocumentoFiscal SET Cancelado = 1, JustificativaCancelamento='ABORTADO VIA BD' WHERE Id = @idDoc;
+DELETE FROM DocumentoFiscalAguardando WHERE Id = @idDoc;
+UPDATE NotaFiscalEletronica SET Status=4 WHERE Id = @idDoc;
+INSERT INTO DocumentoFiscalNaoEnviado values (@idDoc);
